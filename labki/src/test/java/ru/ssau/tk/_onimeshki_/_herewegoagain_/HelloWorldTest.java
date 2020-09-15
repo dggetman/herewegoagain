65fa03decc1bb74786e04357f4dde841460ce6b2
Getman <getman.darya@mail.ru>
2020-09-15 21:51:31 +0400

    Added the HelloWorldTest

---
+++ labki/src/test/java/ru/ssau/tk/_onimeshki_/_herewegoagain_/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package ru.ssau.tk._onimeshki_._herewegoagain_;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class HelloWorldTest {
+
+    private final static double DELTA = 0.00001;
+
+    @Test
+    public void testMain() {
+
+       }
+}
