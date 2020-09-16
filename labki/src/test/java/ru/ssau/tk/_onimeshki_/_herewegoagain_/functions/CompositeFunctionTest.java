f8f8a3124c42b457cca281d121a9d1a3a66f0e30
Getman <getman.darya@mail.ru>
2020-09-16 23:10:23 +0400

    Added method CompositeFunction.

---
+++ labki/src/test/java/ru/ssau/tk/_onimeshki_/_herewegoagain_/functions/CompositeFunctionTest.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk._onimeshki_._herewegoagain_.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CompositeFunctionTest {
+
+    @Test
+    public void testApply() {
+    }
+}
