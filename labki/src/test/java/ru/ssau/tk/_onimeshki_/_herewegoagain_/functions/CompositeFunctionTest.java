158ccd4ebcfc47bca1104cbaf4a6057b4c036e25
Surkin <surkin-ant@mail.ru>
2020-09-17 11:51:08 +0400

    Debug CompositeFunctionTest.

--- labki/src/test/java/ru/ssau/tk/_onimeshki_/_herewegoagain_/functions/CompositeFunctionTest.java
+++ labki/src/test/java/ru/ssau/tk/_onimeshki_/_herewegoagain_/functions/CompositeFunctionTest.java
@@ -3,6 +3,7 @@ package ru.ssau.tk._onimeshki_._herewegoagain_.functions;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 public class CompositeFunctionTest {
     private final static double DELTA = 0.00001;
@@ -12,16 +13,20 @@ public class CompositeFunctionTest {
         MathFunction functionH = new IdentityFunction();
         MathFunction functionG = new SqrtFunction();
         MathFunction functionF = new CompositeFunction(functionH, functionG);
-        assertEquals(functionF.apply(36), 6, DELTA);
         MathFunction abs = new AbsFunction();
         MathFunction sqrt = new SqrtFunction();
-        //MathFunction funcF = new CompositeFunction(sqrt, abs);
-        //assertEquals(funcF.apply(6), Math.sqrt(36), DELTA);
+        assertEquals(functionF.apply(36), 6, DELTA);
+
 
         MathFunction sqr = new SqrFunction();
-        MathFunction composite = abs.andThen(sqrt).andThen(sqr);
-        double result = sqrt.andThen(abs).andThen(sqr).apply(6.66);
+        MathFunction funcF = new CompositeFunction(sqr, sqrt);
+        assertEquals(funcF.apply(6), Math.sqrt(36), DELTA);
 
+
+        MathFunction composite = abs.andThen(sqrt).andThen(sqr);
+        assertEquals(composite.apply(-6.66), 6.66, DELTA);
+        double result = sqrt.andThen(abs).andThen(sqr).apply(6.66);
+        assertEquals(result, 6.66, DELTA);
 
 
     }
