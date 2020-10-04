ac2e13b5b6e6447139ec1feb33b71fd4f0835b8c
Getman <getman.darya@mail.ru>
2020-10-04 18:28:06 +0400

    Added new test AbstractTabulatedFunctionTest.

---
+++ labki/src/test/java/ru/ssau/tk/_onimeshki_/_herewegoagain_/functions/AbstractTabulatedFunctionTest.java
@@ -0,0 +1,33 @@
+package ru.ssau.tk._onimeshki_._herewegoagain_.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+import static ru.ssau.tk._onimeshki_._herewegoagain_.functions.Constants.DELTA;
+
+public class AbstractTabulatedFunctionTest {
+
+    @Test
+    public void testFloorIndexOfX() {
+    }
+
+    @Test
+    public void testExtrapolateLeft() {
+    }
+
+    @Test
+    public void testExtrapolateRight() {
+    }
+
+    @Test
+    public void testInterpolate() {
+    }
+
+    @Test
+    public void testTestInterpolate() {
+    }
+
+    @Test
+    public void testApply() {
+    }
+}
