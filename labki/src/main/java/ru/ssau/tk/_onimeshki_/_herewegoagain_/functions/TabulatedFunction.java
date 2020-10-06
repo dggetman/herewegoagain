58a4851a01346c182a85c92778daae98b3192566
Surkin <surkin-ant@mail.ru>
2020-10-06 22:25:35 +0400

    Relized interface TabulatedFunction.

---
+++ labki/src/main/java/ru/ssau/tk/_onimeshki_/_herewegoagain_/functions/TabulatedFunction.java
@@ -0,0 +1,20 @@
+package ru.ssau.tk._onimeshki_._herewegoagain_.functions;
+
+public interface TabulatedFunction extends MathFunction {
+
+    int getCount();
+
+    double getX(int index);
+
+    double getY(int index);
+
+    void setY(int index, double value);
+
+    int indexOfX(double x);
+
+    int indexOfY(double y);
+
+    double leftBound();
+
+    double rightBound();
+}
